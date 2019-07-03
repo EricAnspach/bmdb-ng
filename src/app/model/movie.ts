ab5899607f53fb886d783ed6fc80b2f049c1c787
Eric Anspach <eric.j.anspach@gmail.com>
2019-07-02 23:52:42 -0400

    Started building Angular project
    Created App Routing module and classes.

---
+++ src/app/model/movie.ts
@@ -0,0 +1,16 @@
+export class Movie {
+    id: number;
+    title: string;
+    rating: string;
+    year: number;
+    director: string;
+
+    constructor (id: number, title: string, rating: string, year: number, director: string) {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.year = year;
+        this.director = director;
+    }
+
+}
