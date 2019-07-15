bcba186c8e2f7b4632737784ebad6b6d8b5b6de3
Eric Anspach <eric.j.anspach@gmail.com>
2019-07-14 23:55:56 -0400

    Actor component work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,22 +24,27 @@ import { CreditListComponent } from './feature/credit/credit-list/credit-list.co
 
 const routes: Routes = [
   {path:'', redirectTo: 'actor/list', pathMatch: 'full' },
+  
   {path:'actor/create', component: ActorCreateComponent},
   {path:'actor/detail', component: ActorDetailComponent},
-  {path:'actor/edit', component: ActorEditComponent},
+  {path:'actor/edit/:id', component: ActorEditComponent},
   {path:'actor/list', component: ActorListComponent},
+
   {path:'genre/create', component: GenreCreateComponent},
   {path:'genre/edit', component: GenreEditComponent},
   {path:'genre/detail', component: GenreDetailComponent},
   {path:'genre/list', component: GenreListComponent},
+
   {path:'credit/create', component: CreditCreateComponent},
   {path:'credit/edit', component: CreditEditComponent},
   {path:'credit/detail', component: CreditDetailComponent},
   {path:'credit/list', component: CreditListComponent},
+
   {path:'movie/create', component: MovieCreateComponent},
   {path:'movie/edit', component: MovieEditComponent},
   {path:'movie/detail', component: MovieDetailComponent},
   {path:'movie/list', component: MovieListComponent},
+
   {path:'moviegenre/create', component: MoviegenreCreateComponent},
   {path:'moviegenre/detail', component: MoviegenreDetailComponent},
   {path:'moviegenre/edit', component: MoviegenreEditComponent},
